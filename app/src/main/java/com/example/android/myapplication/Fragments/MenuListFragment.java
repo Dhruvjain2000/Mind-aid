e9b8f277da580e671f7e6fb7ab28969bad614a87
Dhruv Jain <dhruvjain2212000@gmail.com>
2019-02-08 12:26:58 +0530

    Crashing flowing NAV

---
+++ app/src/main/java/com/example/android/myapplication/Fragments/MenuListFragment.java
@@ -0,0 +1,67 @@
+package com.example.android.myapplication.Fragments;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.design.widget.NavigationView;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.example.android.myapplication.CircleTransformation;
+import com.example.android.myapplication.R;
+import com.squareup.picasso.Picasso;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Activities that contain this fragment must implement the
+ * {@link MenuListFragment.OnFragmentInteractionListener} interface
+ * to handle interaction events.
+ * Use the {@link MenuListFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class MenuListFragment extends Fragment {
+
+    private ImageView ivMenuUserProfilePhoto;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_menu, container,
+                false);
+        ivMenuUserProfilePhoto = (ImageView) view.findViewById(R.id.ivMenuUserProfilePhoto);
+        NavigationView vNavigation = (NavigationView) view.findViewById(R.id.vNavigation);
+        vNavigation.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(MenuItem menuItem) {
+                Toast.makeText(getActivity(),menuItem.getTitle(),Toast.LENGTH_SHORT).show();
+                return false;
+            }
+        }) ;
+        setupHeader();
+        return  view ;
+    }
+
+    private void setupHeader() {
+        int avatarSize = getResources().getDimensionPixelSize(R.dimen.global_menu_avatar_size);
+        String profilePhoto = getResources().getString(R.string.user_profile_photo);
+        Picasso.with(getActivity())
+                .load(profilePhoto)
+                .placeholder(R.drawable.img_circle_placeholder)
+                .resize(avatarSize, avatarSize)
+                .centerCrop()
+                .transform(new CircleTransformation())
+                .into(ivMenuUserProfilePhoto);
+    }
+
+}
