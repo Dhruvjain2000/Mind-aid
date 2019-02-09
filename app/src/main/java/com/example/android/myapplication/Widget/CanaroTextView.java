29bb331d6f7399e8e4424d5bc39c45161684bada
Dhruv Jain <dhruvjain2212000@gmail.com>
2019-02-09 17:42:48 +0530

    Nav added

---
+++ app/src/main/java/com/example/android/myapplication/Widget/CanaroTextView.java
@@ -0,0 +1,24 @@
+package com.example.android.myapplication.Widget;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.TextView;
+
+import com.example.android.myapplication.App;
+
+public class CanaroTextView extends android.support.v7.widget.AppCompatTextView {
+    public CanaroTextView(Context context) {
+        this(context, null);
+    }
+
+    public CanaroTextView(Context context, AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public CanaroTextView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        setTypeface(App.canaroExtraBold);
+    }
+
+}
+
